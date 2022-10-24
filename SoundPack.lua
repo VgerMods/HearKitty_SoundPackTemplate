c3132c4008eaaab7b0069f05b13d53d2d2b5a098
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-10-23 18:38:34 -0700

    Update for Hear Kitty 1.9.6

--- SoundPack.lua
+++ SoundPack.lua
@@ -20,14 +20,26 @@ KittyRegisterSoundPack("Sample_Backwards", {
 	-- TODO: Adjust the length of your sound effects as necessary
 	SoundDelay = 0.35, -- the length of all of your sounds (in seconds) except the "0" sounds
 	LongSoundDelay = 1, -- the length of your "0" sounds
-	
+
 	-- TODO: Include the paths to your sounds here. Note that your sounds will be in your own sound pack's
 	-- folder, not the HearKitty folder. (This sample just reuses the default Symphony sounds.) Note that it's
 	-- possible to reuse the same sound effect for multiple tones. Your sounds can be in Ogg, MP3, or
 	-- uncompressed WAV format.
-	
+
 	-- Combo5StackSound1 = "Interface\\AddOns\\HearKitty_MySoundPackName\\1.mp3",
-	
+
+	-- You MUST include the six Combo5 sounds. If you don't have more sounds to use, Hear Kitty 1.9.6 or later
+	-- will reuse sounds from those if you don't define sounds for resources that stack to 3, 4, 6, or 7.
+
+	Combo7StackSound0 = "Interface\\AddOns\\HearKitty\\Symphony\\0.ogg",
+	Combo7StackSound1 = "Interface\\AddOns\\HearKitty\\Symphony\\5.ogg",
+	Combo7StackSound2 = "Interface\\AddOns\\HearKitty\\Symphony\\4.ogg",
+	Combo7StackSound3 = "Interface\\AddOns\\HearKitty\\Symphony\\3.ogg",
+	Combo7StackSound4 = "Interface\\AddOns\\HearKitty\\Symphony\\2.ogg",
+	Combo7StackSound5 = "Interface\\AddOns\\HearKitty\\Symphony\\1.ogg",
+	Combo7StackSound6 = "Interface\\AddOns\\HearKitty\\Symphony\\2.ogg",
+	Combo7StackSound7 = "Interface\\AddOns\\HearKitty\\Symphony\\1.ogg",
+
 	Combo6StackSound0 = "Interface\\AddOns\\HearKitty\\Symphony\\0.ogg",
 	Combo6StackSound1 = "Interface\\AddOns\\HearKitty\\Symphony\\5.ogg",
 	Combo6StackSound2 = "Interface\\AddOns\\HearKitty\\Symphony\\4.ogg",
@@ -35,20 +47,20 @@ KittyRegisterSoundPack("Sample_Backwards", {
 	Combo6StackSound4 = "Interface\\AddOns\\HearKitty\\Symphony\\2.ogg",
 	Combo6StackSound5 = "Interface\\AddOns\\HearKitty\\Symphony\\1.ogg",
 	Combo6StackSound6 = "Interface\\AddOns\\HearKitty\\Symphony\\2.ogg",
-	
+
 	Combo5StackSound0 = "Interface\\AddOns\\HearKitty\\Symphony\\0.ogg",
 	Combo5StackSound1 = "Interface\\AddOns\\HearKitty\\Symphony\\5.ogg",
 	Combo5StackSound2 = "Interface\\AddOns\\HearKitty\\Symphony\\4.ogg",
 	Combo5StackSound3 = "Interface\\AddOns\\HearKitty\\Symphony\\3.ogg",
 	Combo5StackSound4 = "Interface\\AddOns\\HearKitty\\Symphony\\2.ogg",
 	Combo5StackSound5 = "Interface\\AddOns\\HearKitty\\Symphony\\1.ogg",
-	
+
 	Combo4StackSound0 = "Interface\\AddOns\\HearKitty\\Symphony\\0.ogg",
 	Combo4StackSound1 = "Interface\\AddOns\\HearKitty\\Symphony\\5.ogg",
 	Combo4StackSound2 = "Interface\\AddOns\\HearKitty\\Symphony\\4.ogg",
 	Combo4StackSound3 = "Interface\\AddOns\\HearKitty\\Symphony\\2.ogg",
 	Combo4StackSound4 = "Interface\\AddOns\\HearKitty\\Symphony\\1.ogg",
-	
+
 	Combo3StackSound0 = "Interface\\AddOns\\HearKitty\\Symphony\\0.ogg",
 	Combo3StackSound1 = "Interface\\AddOns\\HearKitty\\Symphony\\5.ogg",
 	Combo3StackSound2 = "Interface\\AddOns\\HearKitty\\Symphony\\4.ogg",
